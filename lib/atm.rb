3419a56810203ce66c4b5ecafc84ce704a166382
Robin Borjesson <robin.b@outlook.com>
2018-08-28 10:50:03 +0200

    Add account parameter to withdraw method

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,12 +1,13 @@
 class ATM
     attr_accessor :funds
     
-    def initialize 
-        @funds =1000
-    end
+    def initialize
+        @funds = 1000
+    end   
 
-    def withdraw(amount)
+    def withdraw(amount, account)
         @funds -= amount
+        account.balance -= amount
+        return  { status: true, message: 'success', date: Date.today, amount: amount }
     end
-
 end
