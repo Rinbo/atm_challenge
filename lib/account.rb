b6c3c6cf022d0a29082bb327c42c28a19fb4ea5e
Robin Borjesson <robin.b@outlook.com>
2018-08-29 13:59:37 +0200

    Add account status and exp date to account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,15 @@
 class Account
-    attr_accessor :pin_code
+    STANDARD_VALIDITY_YRS = 5
+    attr_accessor :pin_code, :exp_date, :account_status
 
     def initialize
         generate_pin
+        set_expire_date
+        @account_status = :active
+    end
+
+    def set_expire_date
+        @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
     private
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,13 +3,23 @@ require 'date'
 
 describe Account do
     
-    let(:person) { instance_double('Person', name: 'Hanna') }
+    #let(:person) { instance_double('Person', name: 'Hanna') }
 
     it 'check length of number' do
         number = subject.pin_code
         number_length = Math.log10(number).to_i + 1
         expect(number_length).to eq 4
     end
+
+    it 'is expcted to have an expiry date on init' do
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
+        expect(subject.exp_date).to eq expected_date
+    end
+
+    it 'is expected to have :active status on initialize' do
+        expect(subject.account_status).to eq :active
+    end
+
 
 
 end
